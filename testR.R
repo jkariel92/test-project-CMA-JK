d53552ebe75cb252395e2078c3619ea6cadac685
Joel Kariel <Joel.Kariel@cma.gov.uk>
2023-06-06 13:34:23 +0100

    plot graph for rajssa

--- testR.R
+++ testR.R
@@ -6,3 +6,8 @@ library(ggplot2)
 
 # read in data
 df = read.csv("test.csv")
+
+# plot the data
+df |> ggplot(aes(x = year, y = markup)) +
+  geom_point(alpha = 0.3) +
+  ggtitle("hi rajssa this is my plot")
