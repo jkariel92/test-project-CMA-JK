13ee3f0522f845bb128219e24ad5860c84220d5c
Joel Kariel <Joel.Kariel@cma.gov.uk>
2023-06-06 13:46:43 +0100

    done some stuff

--- testR.R
+++ testR.R
@@ -3,11 +3,19 @@
 # comment on first branch
 
 library(ggplot2)
+library(dplyr)
 
 # read in data
 df = read.csv("test.csv")
+
+# summarise data
+summary(df)
 
 # plot the data
 df |> ggplot(aes(x = year, y = markup)) +
   geom_point(alpha = 0.3) +
   ggtitle("hi rajssa this is my plot")
+
+# subset data
+df_sub = df |> dplyr::filter(markup > 1 & year > 2000)
+
